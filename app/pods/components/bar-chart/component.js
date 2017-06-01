c38e3bc28f816e15c6d509f2b600ff60d40e2a20
Sam Selikoff <sam.selikoff@gmail.com>
2017-06-01 12:31:01 -0400

    End of part 3

--- app/pods/components/bar-chart/component.js
+++ app/pods/components/bar-chart/component.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import { select } from 'd3-selection';
+import { scaleLinear, scaleBand } from 'd3-scale';
 
 export default Ember.Component.extend({
 
@@ -11,14 +12,25 @@ export default Ember.Component.extend({
   ],
 
   didInsertElement() {
+    let authorCounts = this.get('authors').map(author => author.count);
+    let yScale = scaleLinear()
+      .domain([ 0, Math.max(...authorCounts) ])
+      .range([ 0, 150 ]);
+
+    let xScale = scaleBand()
+      .domain(this.get('authors').map(author => author.name))
+      .range([ 0, 300 ])
+      .paddingInner(0.12);
+
     let svg = select(this.$('svg')[0]);
 
     svg.selectAll('rect').data(this.get('authors'))
       .enter()
       .append('rect')
-      .attr('width', 20)
-      .attr('height', author => author.count)
-      .attr('x', (author, index) => 25 * index);
+      .attr('width', xScale.bandwidth())
+      .attr('height', author => yScale(author.count))
+      .attr('x', author => xScale(author.name))
+      .attr('y', author => 150 - yScale(author.count));
   }
 
 });
