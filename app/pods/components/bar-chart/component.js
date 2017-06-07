14103994cafc86ed80a8fd5743d9450490f5008f
Sam Selikoff <sam.selikoff@gmail.com>
2017-06-06 22:44:55 -0400

    End of part 5

--- app/pods/components/bar-chart/component.js
+++ app/pods/components/bar-chart/component.js
@@ -4,33 +4,28 @@ import { scaleLinear, scaleBand } from 'd3-scale';
 
 export default Ember.Component.extend({
 
-  authors: [
-    { name: 'Mark Twain', count: 15 },
-    { name: 'Virginia Woolf', count: 42 },
-    { name: 'John Steinbeck', count: 23 },
-    { name: 'Ralph Ellison', count: 27 }
-  ],
+  data: [],
 
   didInsertElement() {
-    let authorCounts = this.get('authors').map(author => author.count);
+    let counts = this.get('data').map(data => data.count);
     let yScale = scaleLinear()
-      .domain([ 0, Math.max(...authorCounts) ])
+      .domain([ 0, Math.max(...counts) ])
       .range([ 0, 100 ]);
 
     let xScale = scaleBand()
-      .domain(this.get('authors').map(author => author.name))
+      .domain(this.get('data').map(data => data.label))
       .range([ 0, 100 ])
       .paddingInner(0.12);
 
     let svg = select(this.$('svg')[0]);
 
-    svg.selectAll('rect').data(this.get('authors'))
+    svg.selectAll('rect').data(this.get('data'))
       .enter()
       .append('rect')
       .attr('width', `${xScale.bandwidth()}%`)
-      .attr('height', author => `${yScale(author.count)}%`)
-      .attr('x', author => `${xScale(author.name)}%`)
-      .attr('y', author => `${100 - yScale(author.count)}%`);
+      .attr('height', data => `${yScale(data.count)}%`)
+      .attr('x', data => `${xScale(data.label)}%`)
+      .attr('y', data => `${100 - yScale(data.count)}%`);
   }
 
 });
