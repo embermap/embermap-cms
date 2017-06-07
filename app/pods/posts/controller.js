c4f453cc88e7a1cc9db53614bad8f06430dcbad9
Sam Selikoff <sam.selikoff@gmail.com>
2017-06-06 22:44:55 -0400

    End of part 5

--- app/pods/posts/controller.js
+++ app/pods/posts/controller.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import groupBy from 'ember-group-by';
 
 export default Ember.Controller.extend({
 
@@ -7,6 +8,29 @@ export default Ember.Controller.extend({
     return [ this.get('activeSortBy') ];
   }),
   posts: Ember.computed.sort('model', 'postsSorting'),
+
+  postsByAuthor: groupBy('posts', 'author'),
+  authorData: Ember.computed.map('postsByAuthor', function(group) {
+    return {
+      label: group.value,
+      count: group.items.length
+    };
+  }),
+
+  postsByCategory: groupBy('posts', 'category'),
+  categoryData: Ember.computed.map('postsByCategory', function(group) {
+    return {
+      label: group.value,
+      count: group.items.length
+    };
+  }),
+
+  commentsData: Ember.computed.map('posts', function(post) {
+    return {
+      label: post.get('title'),
+      count: post.get('commentCount')
+    };
+  }),
 
   actions: {
     sort(field) {
