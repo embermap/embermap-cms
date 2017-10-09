56ce2f5d41759b058c5890b30407b5c7555e7500
Sam Selikoff <sam.selikoff@gmail.com>
2017-10-09 10:40:30 -0400

    Beginning

--- app/pods/dashboard/controller.js
+++ app/pods/dashboard/controller.js
@@ -1,21 +1,6 @@
 import Ember from 'ember';
-import { EKMixin } from 'ember-keyboard';
-import { keyUp } from 'ember-keyboard';
 
-export default Ember.Controller.extend(EKMixin, {
-
-  activateKeyboard: Ember.on('init', function() {
-    this.set('keyboardActivated', true);
-  }),
-
-  toggleShowMore: Ember.on(keyUp('KeyR'), function() {
-    console.log('Pressing R');
-    this.set('isLoading', true);
-
-    Ember.run.later(() => {
-      this.set('isLoading', false);
-    }, 2000);
-  }),
+export default Ember.Controller.extend({
 
 
 });
