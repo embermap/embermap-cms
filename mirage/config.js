64da30aa09e1cc54642247dd416c93276a466dd7
Sam Selikoff <sam.selikoff@gmail.com>
2017-11-30 11:45:31 -0500

    End of find by slug

--- mirage/config.js
+++ mirage/config.js
@@ -3,11 +3,26 @@ import { faker } from 'ember-cli-mirage';
 // Lock randomly generated data from faker
 faker.seed(123);
 
+function filterable(resourceName, attrs) {
+  return (schema, request) => {
+    let filters = attrs.reduce((hash, attr) => {
+      let val = request.queryParams[`filter[${attr}]`];
+      if (val) {
+        hash[attr] = val;
+      }
+
+      return hash;
+    }, {});
+
+    return schema[resourceName].where(filters);
+  }
+}
+
 export default function() {
   window.server = this;
 
   this.resource('posts');
 
-  this.get('albums');
-  this.get('images');
+  this.get('albums', filterable('albums', [ 'slug' ]));
+  this.get('images', filterable('images', [ 'slug', 'style' ]));
 }
