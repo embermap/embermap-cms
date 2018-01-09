c9cd70fcfa2c65282b32b085bc6564cd1eed66bf
Sam Selikoff <sam.selikoff@gmail.com>
2018-01-09 14:37:53 -0500

    No polling

--- app/pods/posts/post/route.js
+++ app/pods/posts/post/route.js
@@ -1,5 +1,4 @@
 import Route from '@ember/routing/route';
-import { task, timeout } from 'ember-concurrency';
 
 export default Route.extend({
 
@@ -7,19 +6,6 @@ export default Route.extend({
     return this.get('store').findRecord('post', post_id, {
       include: 'comments,tags,activities'
     });
-  },
-
-  setupController() {
-    this._super(...arguments);
-
-    this.get('poll').perform();
-  },
-
-  poll: task(function * (id) {
-    while (true) {
-      yield timeout(2000);
-      yield this.refresh();
-    }
-  }).cancelOn('deactivate').restartable(),
+  }
 
 });
