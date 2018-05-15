cfd8be9050f02ddb00a143840b1c8fd5b179ad82
Ryan Toronto <ryanto@gmail.com>
2018-05-15 10:21:05 -0400

    rendering 3

--- tests/integration/pods/components/comment-box/component-test.js
+++ tests/integration/pods/components/comment-box/component-test.js
@@ -32,11 +32,11 @@ module('Integration | Component | comment-box', function(hooks) {
     assert.dom('[data-test-id="author"]').includesText('Ryan');
 
     // truncated comments can be expanded
-    await click('[data-test-id="toggle-full-comment"]');
+    await click('[data-test-id="show-more"]');
     assert.dom('[data-test-id="text"]').hasText('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
 
     // expanded comments can be collasped
-    await click('[data-test-id="toggle-full-comment"]');
+    await click('[data-test-id="show-less"]');
     assert.dom('[data-test-id="text"]').hasText('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,...');
   });
 });
