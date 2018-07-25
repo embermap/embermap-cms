c0eb7c0da7e9a185f82a3342ca5b0c133b7e525e
Ryan Toronto <ryanto@gmail.com>
2018-07-25 18:12:04 -0400

    fix event leak

--- app/pods/components/network-status/component.js
+++ app/pods/components/network-status/component.js
@@ -6,10 +6,20 @@ export default Component.extend({
   didInsertElement() {
     this._super(...arguments);
 
-    window.addEventListener('online',  () => { this.updateStatus(); });
-    window.addEventListener('offline', () => { this.updateStatus(); });
+    let _update = () => { this.updateStatus(); };
+    this.set('_update', _update);
+
+    window.addEventListener('online',  _update);
+    window.addEventListener('offline', _update);
 
     this.updateStatus();
+  },
+
+  willDestroyElement() {
+    this._super(...arguments);
+    let _update = this.get('_update');
+    window.removeEventListener('online', _update);
+    window.removeEventListener('offline', _update);
   },
 
   updateStatus() {
