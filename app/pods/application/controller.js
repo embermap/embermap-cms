d7a6f19643677faa55f97961513d8f6e0330afdd
Ryan Toronto <ryanto@gmail.com>
2018-09-27 16:50:06 -0400

    mirage speed control

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -1,6 +1,17 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
-  isShowingNav: true
+  isShowingNav: true,
 
+  isShowingSpeedControl: computed({
+    get() {
+      return window.localStorage.getItem('isShowingSpeedControl') === 'true';
+    },
+
+    set(key, value) {
+      window.localStorage.setItem('isShowingSpeedControl', value);
+      return value;
+    }
+  }),
 });
