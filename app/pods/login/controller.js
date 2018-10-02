58d9672d1be99070d3af9d8359b55b1053f5c9f3
Ryan Toronto <ryanto@gmail.com>
2018-10-02 13:48:59 -0400

    Update to login route

--- app/pods/login/controller.js
+++ app/pods/login/controller.js
@@ -1,6 +1,9 @@
 import Controller from '@ember/controller';
+import { inject as controller } from '@ember/controller';
 
 export default Controller.extend({
+  application: controller(),
+
   actions: {
     login() {
       alert("hello!");
